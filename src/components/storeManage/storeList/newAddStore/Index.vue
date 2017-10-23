ca89d1a8667a7bd6e07fbaa3a4b19bab4f1296b0
ax <ax@axdeMacBook-Air.local>
2017-10-23 17:22:36 +0800

    更新新增门店的验证

--- src/components/storeManage/storeList/newAddStore/Index.vue
+++ src/components/storeManage/storeList/newAddStore/Index.vue
@@ -7,10 +7,8 @@
       <el-row>
         <el-col :span="12" :offset="6">
           <div>
-            <el-form ref="form" :model="form" label-width="100px">
-              <el-form-item
-                prop="name"
-                label="门店名称"
+            <el-form ref="formRules" :model="form" label-width="100px">
+              <el-form-item prop="name" label="门店名称"
                 :rules="[
                   { required: true, message: '请输入门店名称', trigger: 'blur' }
                 ]">
@@ -29,52 +27,49 @@
                   </el-col>
                 </el-row>
               </el-form-item>
-              <el-form-item
-                label="第三方编码"
-                v-for="(domain, index) in form.thirdPartyCoding"
-                :key="domain.key"
-                :prop="'thirdPartyCoding.' + index + '.value'"
-                :rules="{required: true, message: '第三方编码不能为空', trigger: 'blur'}">
-                <div>
-                  <el-row>
-                    <el-col :span="6">
-                      <div>
-                        <el-input v-model="domain.value"></el-input>
-                      </div>
-                    </el-col>
-                    <el-col :span="2">
-                      <div class="m-rank">
-                        <div class="m-rank-child"></div>
-                      </div>
-                    </el-col>
-                    <el-col :span="6">
-                      <div>
-                        <el-input v-model="domain.value"></el-input>
-                      </div>
-                    </el-col>
-                    <el-col :span="6">
-                      <div class="flex_a">
-                        <div class="m-storeCode margin_l_10"  @click="addDomain">
-                          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+              <div v-for="(domain, index) in form.thirdPartyCoding" class="flex_r">
+                <el-form-item label="第三方编码" :key="domain.key" :prop="'thirdPartyCoding.' + index + '.value'"
+                  :rules="{required: true, message: '第三方编码不能为空', trigger: 'blur'}">
+                  <div>
+                    <el-row>
+                      <el-col>
+                        <div style="width:150px">
+                          <el-input v-model="domain.value"></el-input>
                         </div>
-
-                        <div v-if="(form.thirdPartyCoding.length>1) && (index !== 0)" class="m-storeCode margin_l_10" @click.prevent="removeDomain(domain)">
-                          <i class="fa fa-minus-circle" aria-hidden="true"></i>
-                        </div>
-                      </div>
-                    </el-col>
-                  </el-row>
+                      </el-col>
+                    </el-row>
+                  </div>
+                </el-form-item>
+                <div class="m-rank">
+                  <div class="m-rank-child"></div>
                 </div>
-              </el-form-item>
+                <el-form-item label-width="0" :key="domain.key" :prop="'thirdPartyCoding.' + index + '.value1'"
+                  :rules="{required: true, message: '第三方编码不能为空', trigger: 'blur'}">
+                  <div>
+                    <el-row>
+                      <el-col>
+                        <div style="width:150px">
+                          <el-input v-model="domain.value1"></el-input>
+                        </div>
+                      </el-col>
+                    </el-row>
+                  </div>
+                </el-form-item>
+                <div class="flex_sc">
+                  <div class="m-storeCode margin_l_10" @click="addDomain">
+                    <i class="fa fa-plus-circle" aria-hidden="true"></i>
+                  </div>
+                  <div v-if="(form.thirdPartyCoding.length>1) && (index !== 0)" class="m-storeCode margin_l_10"
+                       @click.prevent="removeDomain(domain)">
+                    <i class="fa fa-minus-circle" aria-hidden="true"></i>
+                  </div>
+                </div>
+              </div>
               <el-form-item label="所属门店组:">
                 <el-row>
                   <el-col :span="14">
                     <el-select v-model="storeGroup" filterable placeholder="请选择" style="width: 100%">
-                      <el-option
-                        v-for="item in storeGrounpData"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value">
+                      <el-option v-for="item in storeGrounpData" :key="item.value" :label="item.label" :value="item.value">
                       </el-option>
                     </el-select>
                   </el-col>
@@ -83,31 +78,16 @@
               <el-form-item label="门店地址:">
                 <el-row>
                   <el-col :span="5">
-                    <el-cascader
-                      placeholder="试试搜索：指南"
-                      :options="options"
-                      filterable
-                      change-on-select
-                    ></el-cascader>
+                    <el-cascader placeholder="试试搜索：指南" :options="options" filterable change-on-select></el-cascader>
                   </el-col>
                   <el-col :span="5">
                     <div class="m-store-padding">
-                      <el-cascader
-                        placeholder="试试搜索：指南"
-                        :options="options"
-                        filterable
-                        change-on-select
-                      ></el-cascader>
+                      <el-cascader placeholder="试试搜索：指南" :options="options" filterable change-on-select></el-cascader>
                     </div>
                   </el-col>
                   <el-col :span="5">
                     <div class="m-store-padding-right">
-                      <el-cascader
-                        placeholder="试试搜索：指南"
-                        :options="options"
-                        filterable
-                        change-on-select
-                      ></el-cascader>
+                      <el-cascader placeholder="试试搜索：指南" :options="options" filterable change-on-select></el-cascader>
                     </div>
                   </el-col>
                   <el-col :span="7">
@@ -127,9 +107,8 @@
               </el-form-item>
               <el-form-item>
                 <el-button>取消</el-button>
-                <router-link to="/storeManage/storeList">
-                  <el-button type="primary">保存</el-button>
-                </router-link>
+
+                <el-button type="primary" @click="submitFrom('formRules')">保存</el-button>
               </el-form-item>
             </el-form>
           </div>
@@ -147,7 +126,7 @@
     },
     data() {
       return {
-        navList: [{name: "门店管理", url: ''}, {name: "门店列表", url: '/storeManage/storeList'},{name: "新增门店门店", url: ''}],
+        navList: [{name: "门店管理", url: ''}, {name: "门店列表", url: '/storeManage/storeList'}, {name: "新增门店", url: ''}],
         form: {
           name: '',
           code: '',
@@ -159,9 +138,7 @@
           resource: '',
           desc: '',
           thirdPartyCoding: [
-            {
-              value: ''
-            }
+            {value: '', value1: ''}
           ]
         },
         storeGrounpData: [{
@@ -208,20 +185,33 @@
           value: '',
           key: Date.now()
         });
+      },
+      submitFrom(formRules) {
+        this.$refs[formRules].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+
       }
     },
-    created(){
+    created() {
       console.log(this)
     }
   }
 </script>
 
-<style scoped>
-  .m-rank .m-rank-child {
-    height: 17px;
-    border-bottom: 1px solid #000;
+<style scoped lang="less">
+  .m-rank {
+    width: 40px;
+    .m-rank-child {
+      height: 18px;
+      border-bottom: 1px solid #000;
+    }
   }
-
 
   .m-storeCode {
     font-size: 30px;
